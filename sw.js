659f8a5c12da0ba64dd96535717cf794da267aa5
Justin Ramel <justin.ramel@gmail.com>
2019-09-02 19:46:58 +0100

    533e700

---
+++ sw.js
@@ -0,0 +1,151 @@
+importScripts('/ndca-bar-2019/_nuxt/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/ndca-bar-2019/_nuxt/0909ad4a2e8c7f166ff2.js",
+    "revision": "43ad0c50500523b336b714260f2c524a"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/11235ab7babacb85b8c3.js",
+    "revision": "af2f371415d5b6e129d5e7126f253470"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/13d761d98d0c0196e988.js",
+    "revision": "a6f311cf6c9bb15b13b29b806f16c2d2"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/17011c8494e93fdbe90f.js",
+    "revision": "bfaa2ae3b6089a00f8e18897430dd184"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/1e6854ab19c6db5f716e.js",
+    "revision": "006648efff12570e4450ba948a94d4b3"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/1f998ba4b2c8e40bc4a0.js",
+    "revision": "f4d75bf8c36f64488a919a4c9dd92d44"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/2385a47e36d880f104f3.js",
+    "revision": "223de824edadcc457837dbe34b35bf42"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/2d3be00f83bbb99015be.js",
+    "revision": "5501710aee8b2e63fde57ad3f39011b5"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/353462f03686bcd8de4e.js",
+    "revision": "b7547f55dfa65d7f3418faaa31d4f8d8"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/3921f116e0c72d3c1bb7.js",
+    "revision": "674217b659fab2d693271a3a90e48bca"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/507a44c28fecfff00e7d.js",
+    "revision": "26aff03b2fcff170b06e3e304a9cb80e"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/52243f7ed253f3a67322.js",
+    "revision": "5b8c2fd688ea35de6b049e46d4f29486"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/5dadac3cd455da559d40.js",
+    "revision": "f5caaf07e7369fc4940e141322e287d9"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/6050d966080d87e98e9c.js",
+    "revision": "94719d05d35c35ff80d77c1e2369580f"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/84ee19466b67b0e2b29e.js",
+    "revision": "36eff37b722e07b8830405759918293a"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/85c28351d6fd339e5a52.js",
+    "revision": "1d4e547a804c97489a6c63c2812d0a86"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/8dbbfe110e46297dfaec.js",
+    "revision": "22f33f18e45cc82b8640d7278dd073b0"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/8ddf7682bf7fe4a8bf16.js",
+    "revision": "249652446d78972df94b7c0c0521651b"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/8fcec907b2da7daa61c7.js",
+    "revision": "9b0dda4be4d3165c16a31cdd36c0c489"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/abae37cdf59a65e80fa2.js",
+    "revision": "0dff06e992b0f0b2116bfeeca42fc02d"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/b19de429e8ea150509ae.js",
+    "revision": "b9979b208937e40f978c92839c99202e"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/bc3fa537aec37b60fe15.js",
+    "revision": "b0f5fcec0d817b98395d6ae2ee3fa3f2"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/be7da938f4045fbfe984.js",
+    "revision": "21185148e9e6eb22d6c7ff79686e6ce0"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/be9fdf45ec6b33b822c1.js",
+    "revision": "503fb22313a18d1f8cb37dea4cb50c30"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/c189cbf7af47be5ef75f.js",
+    "revision": "70935d0abf0489a4b3af1e4c2664ce23"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/c376ec19fe182823f87d.js",
+    "revision": "ca6bbb322dcfb62b737415d7bb1fdd2c"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/c9ed2495e65bb3d68ece.js",
+    "revision": "031f3976f6b3f099a33325ab876a17b8"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/d5e07e2846a276de2b67.js",
+    "revision": "f4d418dfecb6741e37d8faefc9b5da10"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/e1ca8934ce08566eaafd.js",
+    "revision": "6037b0b2fd86f8cb6e081a8c504388d7"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/e319ff2731db81eda94b.js",
+    "revision": "86c363e8f76f6ac44e44f86f3a88a826"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/e5e7ebaa6bc126e2c4e3.js",
+    "revision": "a756da66f505918168f9585362c77cc8"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/e703cdcd0b879fc7af27.js",
+    "revision": "ef198a88f46b92a2c258736e24b8839a"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/f350854c91b06b865aae.js",
+    "revision": "bc8637c805ba2dc3054f33bb617d545e"
+  },
+  {
+    "url": "/ndca-bar-2019/_nuxt/f7e2f7dce68d9805f85b.js",
+    "revision": "25e877acc4697740bca4c37dd6de2f47"
+  }
+], {
+  "cacheId": "ndca-bar-2019",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/ndca-bar-2019/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/ndca-bar-2019/.*'), workbox.strategies.networkFirst({}), 'GET')
